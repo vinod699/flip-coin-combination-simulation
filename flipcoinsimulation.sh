82526fd7e3863a0b4047c789da95bca782f7541a
vinod699 <kosarajuvinod9999@gmail.com>
2021-03-05 09:19:02 +0530

    flipcoinsimulation.sh

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -1,11 +1,24 @@
-  
-#!/bin/bash -x
+#!/bin/bash
 
-flip=$((RANDOM%2))
-if [ $flip -eq 0 ]
-then
-	echo heads
-else
-	echo tails
-fi
+H=0
+T=0
+count=10
+
+declare -A coinCombination
+
+for ((i=1; i<=10; i++))
+do
+   flip=$((RANDOM%2))
+   if [ $flip -eq 0 ]
+   then
+      ((H++))
+   else
+      ((T++))
+   fi
+done
+
+coinCombination["H"]=$(($(($H*100))/$count))
+coinCombination["T"]=$(($T*100/count))
+
+echo "percentage of Heads Tails :" ${coinCombination[@]}
 
